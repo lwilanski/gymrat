1511cd790d376b20b0588d69ca9b049fd7bcc905
biganton <134560121+biganton@users.noreply.github.com>
2024-03-08 14:21:40 +0100

    navbar upgrade

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,41 +1,43 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, useTheme } from '@mui/material';
 import { NavLink } from 'react-router-dom';
+import logo from './logo.png'; 
+import Avatar from '@mui/material/Avatar';
 
 const Navbar = () => {
   const theme = useTheme(); // Używa hooka useTheme, aby uzyskać dostęp do motywu
 
+  // Tutaj przykładowy URL zdjęcia zalogowanego użytkownika, w prawdziwej aplikacji 
+  // należy go odpowiednio dostosować do twojego przypadku użycia
+  const userPhotoUrl = "https://example.com/path/to/user/photo.jpg";
+
   const activeStyle = {
-    color: theme.palette.secondary.main, // Poprawnie używa koloru wtórnego z motywu
+    color: theme.palette.secondary.main,
     textDecoration: 'none',
-    margin: '0 10px', // Dodaje trochę przestrzeni między linkami
+    margin: '0 10px',
   };
 
   const linkStyle = {
-    color: theme.palette.primary.contrastText, // Używa kontrastowego tekstu dla koloru pierwotnego
+    color: theme.palette.primary.contrastText,
     textDecoration: 'none',
-    margin: '0 10px', // Podobnie, dodaje przestrzeń
+    margin: '0 10px',
   };
 
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Nazwa Strony
-        </Typography>
-        <NavLink
-          to="/"
-          style={({ isActive }) => (isActive ? activeStyle : linkStyle)}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/workouts"
-          style={({ isActive }) => (isActive ? activeStyle : linkStyle)}
-        >
-          My Workouts
-        </NavLink>
-        {/* Możesz dodać więcej linków tutaj, zgodnie z potrzebą */}
+      <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <NavLink to="/">
+            <img src={logo} alt="Logo" style={{ width: '100px', height: '100px' }}/>
+          </NavLink>
+          <NavLink to="/" style={({ isActive }) => (isActive ? activeStyle : linkStyle)}>Home</NavLink>
+          <NavLink to="/workouts" style={({ isActive }) => (isActive ? activeStyle : linkStyle)}>My Workouts</NavLink>
+          <NavLink to="/exercises" style={({ isActive }) => (isActive ? activeStyle : linkStyle)}>Exercises</NavLink>
+          <NavLink to="/progress" style={({ isActive }) => (isActive ? activeStyle : linkStyle)}>Progress</NavLink>
+        </div>
+
+        {/* Avatar użytkownika */}
+        <Avatar src={userPhotoUrl} alt="User Photo" sx={{ bgcolor: theme.palette.secondary.main }} />
       </Toolbar>
     </AppBar>
   );
